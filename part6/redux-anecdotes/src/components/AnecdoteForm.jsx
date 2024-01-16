a16ef8104ec630eca0b5ea22b864d62125c628a0
Tomas Munill Mazzalay <tomasmunill17@gmail.com>
2024-01-16 10:19:23 -0300

    ej 6.3-6.8

---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,27 @@
+import { useDispatch } from "react-redux";
+import { add } from "../reducers/anecdoteReducer";
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch();
+
+  const createAnecdote = () => {
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    event.target.anecdote.value = "";
+    dispatch(add(content));
+  };
+
+  return (
+    <>
+      <h2>create new</h2>
+      <form onSubmit={createAnecdote}>
+        <div>
+          <input name="anecdote" />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </>
+  );
+};
+
+export default AnecdoteForm;
