9ee3cdf370cb1acfe28175554c1d79c78e946ef4
Tomas Munill Mazzalay <tomasmunill17@gmail.com>
2024-01-16 12:04:16 -0300

    ej 6.9

---
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -0,0 +1,20 @@
+export const filterChange = filter => {
+    return {
+        type: 'SET_FILTER',
+        filter: filter
+    }
+}
+
+const reducer = (state = '', action) => {
+  console.log('state now: ', state)
+  console.log('action', action)
+
+  switch (action.type) {
+    case 'SET_FILTER': 
+      return action.filter
+    default:
+      return state
+  }
+}
+
+export default reducer
