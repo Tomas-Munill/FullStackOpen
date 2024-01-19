bb10c1aaffcd1d00fa29e52ad36340eda96cbc33
Tomas Munill Mazzalay <tomasmunill17@gmail.com>
2024-01-19 11:42:55 -0300

    ej 6.20-6.22

---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,57 @@
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+import { useQuery, useQueryClient, useMutation } from 'react-query'
+import { getAnecdotes, updateAnecdote } from './requests'
+
+const App = () => {
+  const queryClient = useQueryClient()
+  const result = useQuery('anecdotes', getAnecdotes)
+
+  const updateAnecdoteMutation = useMutation(updateAnecdote, {
+    onSuccess: (updateAnecdote) => {
+      const anecdotes = queryClient.getQueryData('anecdotes')
+      queryClient.setQueryData('anecdotes', 
+      anecdotes.map(a => a.id === updateAnecdote.id 
+        ? updateAnecdote 
+        : a))
+    },
+  })
+
+  const handleVote = (anecdote) => {
+    updateAnecdoteMutation.mutate({...anecdote, votes: anecdote.votes+1})
+  }
+  
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+
+  if (result.isError) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  const anecdotes = result.data
+  console.log(result)
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+    
+      <Notification />
+      <AnecdoteForm />
+    
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
